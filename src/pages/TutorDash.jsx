1094449f6232dabe84415cc1b22ccf7a667edfc1
Alice Dawson <Alice Dawson>
2025-04-11 11:36:49 +0000

    pages for tutors connect

---
+++ src/pages/TutorDash.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TutorDash = () => {
+  return <div>TutorDash</div>;
+};
+
+export default TutorDash;
---
+++ src/pages/TutorProfile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TutorProfile = () => {
+  return <div>TutorProfile</div>;
+};
+
+export default TutorProfile;
